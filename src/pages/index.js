72db21eddc29c9cc472fa4c7affa86bf2fb49248
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-02 11:28:14 +1000

    Remove <Image>
    I'm probably going to have to add that back later,
    but that's what git's for. #CodeFearlessly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
-
 import Section from "../components/Layout/Section";
+import "./../components/layout.scss";
 
 const IndexPage = () => (
   <>
@@ -12,8 +9,6 @@ const IndexPage = () => (
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
-
-      <Image />
     </Section>
     <Section>
       <h1>Yeah Nice</h1>
