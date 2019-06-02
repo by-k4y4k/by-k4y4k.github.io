bb07cab675d4d9d3747e2e0f0e5b1193a311e5fe
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-02 10:49:40 +1000

    Configure SCSS instead of CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `<kayak />`,
+    description: `Portfolio Site`,
+    author: `@by_kayak`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,24 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`900i`],
+          },
+          {
+            family: `Fira Sans`,
+            variants: [`400`, `400i`],
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
