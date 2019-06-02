050fbd4f6395bcd1e2e0346ff0c480cccdd499b0
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-02 11:26:41 +1000

    This is a single page site

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,21 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
+import Section from "../components/Layout/Section";
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  <>
+    <Section>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+
       <Image />
-    </div>
-  </Layout>
+    </Section>
+    <Section>
+      <h1>Yeah Nice</h1>
+    </Section>
+  </>
 );
 
 export default IndexPage;
