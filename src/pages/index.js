282e73be5b13a76e0751e0f2b3b4782f8ab3abfd
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-05 15:25:58 +1000

    Add <BackgroundSection>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 import Section from "../components/Layout/Section";
+import BackgroundSection from "../components/Layout/BackgroundImageSection";
 import "./../components/layout.scss";
 
 const IndexPage = () => (
   <>
-    <Section>
+    <BackgroundSection>
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
-    </Section>
+    </BackgroundSection>
     <Section>
       <h1>Yeah Nice</h1>
     </Section>
+ 
   </>
 );
 
