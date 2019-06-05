cece53f253a75d59d933f80cd022cea8bea52cc4
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-05 21:34:16 +1000

    Add <HeroContainer>

---
+++ src/components/Layout/HeroContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeroContainer = ({ className, children }) => {
+  return <div className={className}>{children}</div>;
+};
+
+const StyledHeroContainer = styled(HeroContainer)`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export default StyledHeroContainer;
