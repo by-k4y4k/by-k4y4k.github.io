e43c8f3b5b4e47164e4c98d7b5890efec100eda0
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-05 21:42:01 +1000

    Add <HeroType>

---
+++ src/components/Typography/Hero/HeroType.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeroType = ({ className, children }) => {
+  return <h1 className={className}>{children}</h1>;
+};
+
+const StyledHeroType = styled(HeroType)`
+  color: white;
+  text-align: center;
+  align-self: center;
+  justify-content: center;
+  font-size: 12.5vw;
+`;
+
+export default StyledHeroType;
