ad41da50440766393525b9078f21e00278092ecf
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-08 17:48:04 +1000

    Adjust <Section>
    - Add styling via styled-components

--- src/components/Layout/Section.js
+++ src/components/Layout/Section.js
@@ -1,11 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
-const Section = props => {
-  console.log(props);
+const Section = ({ className, children }) => {
   return (
     <section style={{ width: "100vw", minHeight: "100vh" }}>
       {props.children}
     </section>
   );
 };
-export default Section;
+
+const StyledSection = styled(Section)`
+  width: 100vw;
+  min-height: 100vh;
+`;
+
+export default StyledSection;
