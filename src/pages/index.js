4b205b20ccbec5765609a2d7f6222c275affbec6
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-21 22:45:41 +1000

    Add <HireSectionContent>
      This component reads `./src/content/hire.md`. Fixes #14.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import HeroType from '../components/Typography/Hero/HeroType';
 
 // Content imports
 import AboutSectionContent from '../components/Content/AboutSectionContent';
+import HireSectionContent from '../components/Content/HireSectionContent';
 
 import './../components/layout.scss';
 import styled from 'styled-components';
@@ -55,6 +56,7 @@ const IndexPage = ({className}) => (
     </Section>
     <Section>
       <h1 id='hire'>Hire</h1>
+      <HireSectionContent></HireSectionContent>
     </Section>
     <Section>
       <h1 id='blog'>Blog</h1>
