a5aa3604a341cdaad0e5e3925d406e9d94bf20ed
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-21 14:29:16 +1000

    Read Markdown into <AboutSectionContent>

--- src/components/Content/AboutSectionContent.js
+++ src/components/Content/AboutSectionContent.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import {useStaticQuery, graphql} from 'gatsby';
 
-const AboutSectionContent = ({className, children}) => {
-  return <p className={className}>content</p>;
+const AboutSectionContent = ({className}) => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(filter: { frontmatter: { title: { eq: "about" } } }) {
+        edges {
+          node {
+            html
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <div
+      className={className}
+      dangerouslySetInnerHTML={{
+        __html: data.allMarkdownRemark.edges[0].node.html,
+      }}
+    />
+  );
 };
 
 const StyledAboutSectionContent = styled(AboutSectionContent)``;
