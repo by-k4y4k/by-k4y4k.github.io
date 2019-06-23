1001eae63a80a027e3f3ea8a735700a2d8f821b6
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-23 20:37:55 +1000

    Update layout
      Add <IteratedBlogImageContainer> to map() call in the blogroll.

--- src/components/Content/BlogContentList.js
+++ src/components/Content/BlogContentList.js
@@ -5,6 +5,7 @@ import {useStaticQuery, graphql} from 'gatsby';
 import IteratedBlogContainer from '../Layout/Blogroll/IteratedBlogContainer';
 // eslint-disable-next-line
 import IteratedBlogTextContainer from '../Layout/Blogroll/IteratedBlogTextContainer';
+import IteratedBlogImageContainer from '../Layout/Blogroll/IteratedBlogImage';
 
 const BlogContentList = ({className, children}) => {
   const data = useStaticQuery(graphql`
@@ -36,6 +37,7 @@ const BlogContentList = ({className, children}) => {
           className={className}
           key={node.childMarkdownRemark.id}
         >
+          <IteratedBlogImageContainer></IteratedBlogImageContainer>
           <IteratedBlogTextContainer>
             <h4>{node.childMarkdownRemark.frontmatter.title}</h4>
             <p>Posted on {node.childMarkdownRemark.frontmatter.date}</p>
