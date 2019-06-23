3c952e0a09e9bd44b9eb5809b25eba5d9e6192c2
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-23 19:50:59 +1000

    Add <IteratedBlogTextContainer>
      <IBTC> is a container for the text of blog roll entries.

---
+++ src/components/Layout/Blogroll/IteratedBlogTextContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const IteratedBlogTextContainer = ({className, children}) => {
+  return <div className={className}>{children}</div>;
+};
+
+const StyledIteratedBlogTextContainer = styled(IteratedBlogTextContainer)``;
+
+export default StyledIteratedBlogTextContainer;
