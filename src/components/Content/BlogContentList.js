9f94572874d46f590babb67ba9c7379ea8f49324
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-23 18:19:45 +1000

    Update <BlogContentList>
    - Added query for all Markdown files in `./src/content/blog`
    - Map all query results into <IteratedBlogContainer>
    - Added styling

--- src/components/Content/BlogContentList.js
+++ src/components/Content/BlogContentList.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import {useStaticQuery, graphql} from 'gatsby';
 
+import IteratedBlogContainer from '../Layout/Blogroll/IteratedBlogContainer';
+ 
 const BlogContentList = ({className, children}) => {
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: { relativePath: { glob: "*/blog/*" } }) {
+        edges {
+          node {
+            childMarkdownRemark {
+              frontmatter {
+                title
+                date
+              }
+              excerpt(format: PLAIN, pruneLength: 33, truncate: true)
+              id
+            }
+          }
+        }
+        totalCount
+      }
+    }
+  `);
+
+  console.log(data.allFile);
+
   return (
-    <div className={className}>
-      <p>Blog Content Will Go Here</p>
-    </div>
+    <>
+      {data.allFile.edges.map(({node}) => (
+        <IteratedBlogContainer
+          className={className}
+          key={node.childMarkdownRemark.id}
+        >
+          <h4>{node.childMarkdownRemark.frontmatter.title}</h4>
+          <p>Posted on {node.childMarkdownRemark.frontmatter.date}</p>
+          <p>{node.childMarkdownRemark.excerpt}</p>
+        </IteratedBlogContainer>
+      ))}
+    </>
   );
 };
 
-const StyledBlogContentList = styled(BlogContentList)``;
+const StyledBlogContentList = styled(BlogContentList)`
+  border-bottom: 3px solid #915fee;
+`;
 
 export default StyledBlogContentList;
