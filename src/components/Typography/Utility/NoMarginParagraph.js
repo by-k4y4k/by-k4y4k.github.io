6c078e225991e02d5090dd4e16d70b6e3db90144
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-24 12:54:45 +1000

    Replace card styles with generic ones
      All that's happening in the <CardContainer> component is <p> and
    <h?> tags with no bottom margin. Therefore, those components could be
    named more generically.

---
+++ src/components/Typography/Utility/NoMarginParagraph.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NoMarginParagraph = ({className, children}) => {
+  return <p className={className}>{children}</p>;
+};
+
+const StyledNoMarginParagraph = styled(NoMarginParagraph)`
+  margin-bottom: 0;
+`;
+
+export default StyledNoMarginParagraph;
