9cbedd5217e14a318c02945ce6f32085d3775251
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-24 17:36:25 +1000

    Add basic scaffolding for blog pages
      There are two steps to Gatsby's creation of standalone pages (taken
    from the Gatsby tutorial part 7):
    
    1. Querying data with GraphQL
    2. Mapping the query results to pages
    
    `gatsby-node.js` contains the query, and it passes results to
    `BlogPostPage.js`. It's like a huge abstracted map() call!

---
+++ src/templates/BlogPostPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link, graphql} from 'gatsby';
+
+import Section from '../components/Layout/Section';
+
+const BlogPostPage = ({data}) => {
+  const post = data.markdownRemark;
+  return (
+    <main>
+      <Section>
+        <p>
+          <Link to='/'>{'<< go back'}</Link>
+        </p>
+      </Section>
+      
+      <Section>
+        <div>
+          <h1>{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.html}} />
+        </div>
+      </Section>
+    </main>
+  );
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
+
+export default BlogPostPage;
