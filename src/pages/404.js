0f61d62c8f5a293e709c4fbeef0fc2051383393c
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-24 18:39:15 +1000

    Add link to index to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,16 @@ import SEO from '../components/seo';
 
 import Section from '../components/Layout/Section';
 import './../components/layout.scss';
+import {Link} from 'gatsby';
 
 const NotFoundPage = () => (
   <Section>
     <SEO title='404: Not found' />
     <h1>WHUH OH</h1>
     <p>Yeah, nah, that page isn&apos;t here.</p>
+    <p>
+      <Link to='/'>{'<< go back'}</Link>
+    </p>
   </Section>
 );
 
