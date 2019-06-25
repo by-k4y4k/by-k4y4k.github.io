5dc3e1876cc79c9735aa8f89e71878a3fb5ad9ff
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-25 12:25:01 +1000

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,27 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`,
       },
     },
-    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 960,
+              linkImagesToOriginal: true,
+              showCaptions: [`alt`, `title`],
+              withWebp: true,
+              backgroundColor: `#0000000`,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
