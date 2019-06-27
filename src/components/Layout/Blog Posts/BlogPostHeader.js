64964871458225028328fa9bc8550dd0efe8f87c
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-27 20:54:16 +1000

    String together header from query data

--- src/components/Layout/Blog Posts/BlogPostHeader.js
+++ src/components/Layout/Blog Posts/BlogPostHeader.js
@@ -3,13 +3,24 @@ import styled from 'styled-components';
 import {Link} from 'gatsby';
 
 import Section from '../Section';
+import NoMarginParagraph from '../../Typography/Utility/NoMarginParagraph';
 
 const BlogPostHeader = ({className, children, data}) => {
+  console.log(data);
   return (
     <Section>
       <p>
-        <Link to='/#blog'>{'<< go back '}</Link>
+        <code>{data.markdownRemark.parent.name}.md</code>
+        {` - `}
+        <code>{data.markdownRemark.timeToRead} minute read</code>
+        {` - `}
+        <code>{data.markdownRemark.frontmatter.date}</code>
+        {` - `}
+        <code>{data.markdownRemark.parent.prettySize}</code>
       </p>
+      <NoMarginParagraph>
+        <kbd>$ cd ../</kbd>
+      </NoMarginParagraph>
     </Section>
   );
 };
