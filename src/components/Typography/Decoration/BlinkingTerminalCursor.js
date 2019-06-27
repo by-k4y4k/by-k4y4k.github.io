2b565bff050da8990b5fc9d875e8e7c2a2921236
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-27 21:36:21 +1000

    Add <BlinkingTerminalCursor>

---
+++ src/components/Typography/Decoration/BlinkingTerminalCursor.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+
+const BlinkingTerminalCursor = ({className, children}) => {
+  return (
+    <span className={className}>
+      ▎
+    </span>
+  );
+};
+
+// Create the keyframes
+const blinkKeyframes = keyframes`
+  from, to {
+    color: transparent;
+  }
+  50% {
+    color: inherit;
+  }
+`;
+
+const StyledBlinkingTerminalCursor = styled(BlinkingTerminalCursor)` 
+  display: inline-block;
+  animation: ${blinkKeyframes} 1.5s infinite;
+`;
+
+
+export default StyledBlinkingTerminalCursor;
