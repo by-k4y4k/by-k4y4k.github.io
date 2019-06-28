a9b459d87fde4feed5ed85040540d78268b2f298
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-06-28 18:27:08 +1000

    Add CSS theme for prism

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,8 @@
 
 // You can delete this file if you're not using it
 
+require('prismjs/themes/prism-tomorrow.css');
+
 // eslint-disable-next-line
 const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
