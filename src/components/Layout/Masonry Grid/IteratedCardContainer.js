430644fa0b962f410724f1073d498f6501b54f2a
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-07-02 14:48:18 +1000

    Update component - remove hard coded items

--- src/components/Layout/Masonry Grid/IteratedCardContainer.js
+++ src/components/Layout/Masonry Grid/IteratedCardContainer.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import CardImage from './IteratedCardImage.js';
-import NoMarginHeader from '../../Typography/Utility/NoMarginHeader';
-import NoMarginParagraph from '../../Typography/Utility/NoMarginParagraph';
-
-const CardContainer = ({className}) => {
-  return (
-    <div className={className}>
-      <CardImage />
-      <NoMarginHeader>Card Header</NoMarginHeader>
-      <NoMarginParagraph>
-        Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast
-        yardarm.
-      </NoMarginParagraph>
-      <NoMarginParagraph accent='true'>HTML, CSS, JavaScript</NoMarginParagraph>
-    </div>
-  );
+const CardContainer = ({className, children}) => {
+  return <div className={className}>{children}</div>;
 };
 
 const StyledCardContainer = styled(CardContainer)`
