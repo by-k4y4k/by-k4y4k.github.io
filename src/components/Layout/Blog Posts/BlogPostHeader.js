52002736a7073883691e960f0d4d868e2762e9bb
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-07-04 21:14:51 +1000

    Update component

--- src/components/Layout/Blog Posts/BlogPostHeader.js
+++ src/components/Layout/Blog Posts/BlogPostHeader.js
@@ -4,22 +4,18 @@ import {Link} from 'gatsby';
 import Section from '../Section';
 import NoMarginParagraph from '../../Typography/Utility/NoMarginParagraph';
 // eslint-disable-next-line
-import BlinkingTerminalCursor from "../../Typography/Decoration/BlinkingTerminalCursor";
+import BlinkingTerminalCursor from '../../Typography/Decoration/BlinkingTerminalCursor';
 
-const BlogPostHeader = ({data, location}) => {
+const BlogPostHeader = ({data}) => {
   return (
     <Section>
       <NoMarginParagraph>
         <code>{data.markdownRemark.parent.name}.md</code>
-        {` - `}
         <code>{data.markdownRemark.timeToRead} minute read</code>
-        {` - `}
         <code>{data.markdownRemark.frontmatter.date}</code>
-        {` - `}
         <code>{data.markdownRemark.parent.prettySize}</code>
-        {` - `}
         <code>
-          <Link to="/#work">
+          <Link to='/#work'>
             <kbd>$ cd ../ && ls </kbd>
             <BlinkingTerminalCursor />
           </Link>
@@ -28,6 +24,5 @@ const BlogPostHeader = ({data, location}) => {
     </Section>
   );
 };
-
 
 export default BlogPostHeader;
