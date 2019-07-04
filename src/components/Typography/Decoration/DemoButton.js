9e54b1ed15461aecb97dd20fa0b6f717a7ebe6f3
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-07-04 20:41:34 +1000

    Update component

--- src/components/Typography/Decoration/DemoButton.js
+++ src/components/Typography/Decoration/DemoButton.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { FiGlobe } from 'react-icons/fi';
+import {FiGlobe} from 'react-icons/fi';
+import {IconContext} from 'react-icons';
 
 const DemoButton = ({className, children, demoLink}) => {
   return (
-    <a className={className} href={demoLink}>
-      <FiGlobe /> Demo
-    </a>
+    <IconContext.Provider value={{className: 'react-icons'}}>
+      <a className={className} href={demoLink}>
+        <FiGlobe /> Demo
+      </a>
+    </IconContext.Provider>
   );
 };
 
-const StyledDemoButton = styled(DemoButton)``;
+const StyledDemoButton = styled(DemoButton)`
+  font-size: 1.8em;
+  font-family: 'Fira Sans';
+  text-transform: uppercase;
+  border: 1px solid #915fee;
+  padding: 5px 10px;
+  margin: 0 5px;
+  letter-spacing: 3px;
+`;
 
 export default StyledDemoButton;
--- src/components/Typography/Decoration/GitHubButton.js
+++ src/components/Typography/Decoration/GitHubButton.js
@@ -2,15 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 import {FiGithub} from 'react-icons/fi';
+import {IconContext} from 'react-icons';
 
 const GitHubButton = ({className, children, githubLink}) => {
   return (
-    <a className={className} href={githubLink}>
-      <FiGithub /> GitHub
-    </a>
+    <IconContext.Provider value={{className: 'react-icons'}}>
+      <a className={className} href={githubLink}>
+        <FiGithub /> GitHub
+      </a>
+    </IconContext.Provider>
   );
 };
 
-const StyledGitHubButton = styled(GitHubButton)``;
+const StyledGitHubButton = styled(GitHubButton)`
+  font-size: 1.8em;
+  font-family: 'Fira Sans';
+  text-transform: uppercase;
+  border: 1px solid #915fee;
+  padding: 5px 10px;
+  margin: 0 5px 0 0;
+  letter-spacing: 3px;
+`;
 
 export default StyledGitHubButton;
