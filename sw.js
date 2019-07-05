7e4bd03c203dbf82eac5ae587c01ce7f9637c730
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-07-05 15:42:21 +1000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e1f45da80bbb327deaea.js"
+    "url": "webpack-runtime-a483463d5cca67993cfb.js"
   },
   {
     "url": "styles.066846eac59eb1ebce12.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f269d221531306ce7f1db84f97ce7270"
+    "revision": "f747e9c924307e3d3f59204e01e04d50"
   },
   {
     "url": "1-8be92ea380fe57056e3b.js"
