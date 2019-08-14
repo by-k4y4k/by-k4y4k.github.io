6919944fcff9fecf90df2bd0f79947cd7e55e7d6
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-08-14 15:36:53 +1000

    Update logic of card button generator
      Sometimes I might now want to share my code. Or a demo. Now,
    <IteratedCardButtons> won't short out if that's the case.

--- src/components/Layout/Masonry Grid/IteratedCardButtons.js
+++ src/components/Layout/Masonry Grid/IteratedCardButtons.js
@@ -1,19 +1,33 @@
 import styled from 'styled-components'
 import React from 'react'
 
-import StyledGitHubButton from '../../Typography/Decoration/GitHubButton'
-import StyledDemoButton from '../../Typography/Decoration/DemoButton'
+import GitHubButton from '../../Typography/Decoration/GitHubButton'
+import DemoButton from '../../Typography/Decoration/DemoButton'
+
 
 const IteratedCardButtons = ({className, children, github, demo}) => {
-  if (!github || !demo) return null
+  if ((!github && !demo) || (github === null && demo === null)) return null
 
   const githubLink = github.replace(/[<>]/g, '')
   const demoLink = demo.replace(/[<>]/g, '')
 
+  // TODO: refactor this
+  if (!githubLink || githubLink === null) {
+    return (<div className={className}>
+      <DemoButton demoLink={demoLink}></DemoButton>
+    </div>)
+  }
+
+  if (!demoLink || demoLink === null) {
+    return (<div className={className}>
+      <GitHubButton githubLink={githubLink}></GitHubButton>
+    </div>)
+  }
+
   return (
     <div className={className}>
-      <StyledGitHubButton githubLink={githubLink} />
-      <StyledDemoButton demoLink={demoLink} />
+      <GitHubButton githubLink={githubLink} />
+      <DemoButton demoLink={demoLink} />
     </div>
   )
 }
